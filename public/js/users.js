f3b119f00dcd90330a7e90925a45dba4461acce9
gab1s3ll13r-alt <gab1.s3ll13r@gmail.com>
2026-06-13 22:09:31 +0200

    users.js

--- public/js/users.js
+++ public/js/users.js
@@ -1,23 +1,8 @@
-/**
- * public/js/users.js
- * Recherche et identification des utilisateurs.
- * Basé sur les routes :
- * - GET /api/users/search
- * - GET /api/users/lookup
- */
-
 import api from './api.js';
 
 const users = {
-    // Recherche partielle par nom ou IBAN
-    async search(query) {
-        return await api.users.search(query);
-    },
-
-    // Vérification d'un IBAN complet pour un virement
-    async lookupIban(iban) {
-        return await api.users.lookup(iban);
-    }
+    search: (q) => api.users.search(q),
+    lookup: (iban) => api.users.lookup(iban)
 };
 
 export default users;
